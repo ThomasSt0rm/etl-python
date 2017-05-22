2472093374fb8f3c5a6a008752da01a73d9922af
Thomas St0rm <zezr0@live.com>
2017-05-22 21:01:01 +0200

    add csv loader

--- app.py
+++ app.py
@@ -8,13 +8,11 @@ import extractor
 import loader
 import transformer
 
-raw_number = input("Enter the number to transform: ")
-#structure = extractor.Extract(raw_number)
-structure = extractor.Extract()
-extracted_number = structure.extract(raw_number)
-#print(extracted_number)
-transform_step = transformer.Transform()
-transformed_number = transform_step.transform(extracted_number)
-loading_step = loader.Load()
-loaded_number = loading_step.load(transformed_number)
-print loaded_number
+#raw_number = input("Enter the number to transform: ")
+#structure = extractor.Extract_Basic()
+#extracted_number = structure.extract(raw_number)
+#transform_step = transformer.Transform()
+#transformed_number = transform_step.transform(extracted_number)
+#loading_step = loader.Load()
+#loaded_number = loading_step.load(transformed_number)
+#print loaded_number
--- extractor/extractor.py
+++ extractor/extractor.py
@@ -1,8 +1,20 @@
-class Extract:
+import csv
+import numpy
+
+class Extract_Basic:
     def __init__(self):
-        #self.number = number
         pass
 
     def extract(self, number):
         self.number = number
         return number
+
+class Extract_CSV:
+    def __init__(self):
+        pass
+
+    def read_csv(self, csvfile):
+        self.csvfile = csvfile
+        reader = csv.reader(open(csvfile, 'rb'), delimiter=',')
+        x = list(reader)
+        result = numpy.array(x).astype(float)
