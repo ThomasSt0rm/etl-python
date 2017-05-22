924bdb3dca9a1c09954107b221f88f58b97a39aa
ThomasSt0rm <zezr0@live.com>
2017-05-22 15:50:47 +0200

    Update transformer.py

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -1,8 +1,9 @@
 class Transform:
-    def __init__(self, number):
-        self.number = number
+    def __init__(self):
+        #self.number = number
+        pass
 
     def transform(self, number):
-        self.number = number
-        new_number = self.number + 1
-        return new_number
+        #self.number = number
+        #new_number = self.number + 1
+        return number + 1
