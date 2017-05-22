1512bd99d0287553e16126b9a29b5772b737e1db
ThomasSt0rm <zezr0@live.com>
2017-05-22 15:51:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,12 @@ import loader
 import transformer
 
 raw_number = input("Enter the number to transform: ")
-structure = extractor.Extract(raw_number)
-structure.print_value()
-
+#structure = extractor.Extract(raw_number)
+structure = extractor.Extract()
 extracted_number = structure.extract(raw_number)
 #print(extracted_number)
-transform_step = transformer.Transform(extracted_number)
+transform_step = transformer.Transform()
 transformed_number = transform_step.transform(extracted_number)
-loading_step = loader.Load(transformed_number)
+loading_step = loader.Load()
 loaded_number = loading_step.load(transformed_number)
 print loaded_number
