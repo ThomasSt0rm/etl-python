ba6a4dbd4bc0447c7e19aaf1170ed2f8ed35b9d7
Karen Tovmasian <k.tovmasian@coolblue.nl>
2017-05-23 17:47:33 +0200

    loader

--- loader/loader.py
+++ loader/loader.py
@@ -7,7 +7,22 @@ class Load_CSV:
     def load(self, data, outfile):
         self.outfile = outfile
         self.data = data
-        with open(outfile, 'wb') as f:
-            w = csv.DictWriter(f, data.keys())
+        with open(outfile, 'w') as f:
+            fields = ['ORDER_ID', 'ORDER_LINE_ID', 'ORDER_LINE_ITEM_ID','GROSS_MARGIN']
+            w = csv.DictWriter(f, fieldnames=['ORDER_ID', 'ORDER_LINE_ID', 'ORDER_LINE_ITEM_ID','GROSS_MARGIN'], extrasaction='ignore', delimiter = ',')
             w.writeheader()
-            w.writerow(data)
+        #    print(data)
+            for item in data:
+                w.writerow(item)
+        #    count = 0
+        #    writer = csv.writer(f, dialect="excel-tab")
+        #    for row in data:
+        #        if count == 0:
+        #            header=row.keys()
+        #            writer.writerow(header)
+        #            count=+1
+        #        writer.writerow(row.values())
+        #CSV ="\n".join([k+','+",".join(v) for k,v in data.items()]) 
+        #print CSV
+        #with open(outfile, "w") as file:
+        #    file.write(CSV)
